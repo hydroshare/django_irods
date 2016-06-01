3294ece0ef548496728e48f29c4845b456e84c4c
hyi <hongyi@renci.org>
2016-06-01 18:18:14 -0400

    added code to handle resource download from federated zone

--- views.py
+++ views.py
@@ -12,14 +12,43 @@ from django.http import HttpResponse, FileResponse
 
 from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
 from hs_core.hydroshare.hs_bagit import create_bag_by_irods
+from hs_core.hydroshare.utils import set_fed_zone_session
 from . import models as m
 from .icommands import Session, GLOBAL_SESSION
 
 
 @api_view(['GET'])
 def download(request, path, *args, **kwargs):
-    
-    split_path_strs = path.split('/')
+    istorage = IrodsStorage()
+    idx = -1
+    federated_path = ''
+    if settings.HS_LOCAL_PROXY_USER_IN_FED_ZONE:
+        idx = path.find(settings.HS_LOCAL_PROXY_USER_IN_FED_ZONE)
+    if idx > 0:
+        # the resource is stored in federated zone
+        set_fed_zone_session(istorage)
+        session = icommands.ACTIVE_SESSION
+        s_idx = idx + len(settings.HS_LOCAL_PROXY_USER_IN_FED_ZONE)+1
+        rel_path = path[s_idx:]
+        split_path_strs = rel_path.split('/')
+        # prepend / so that path for federated zone is absolute path
+        path = '/{}'.format(path)
+        federated_path = path[:s_idx]
+    else:
+        split_path_strs = path.split('/')
+        if 'environment' in kwargs:
+            environment = int(kwargs['environment'])
+            environment = m.RodsEnvironment.objects.get(pk=environment)
+            session = Session("/tmp/django_irods", settings.IRODS_ICOMMANDS_PATH, session_id=uuid4())
+            session.create_environment(environment)
+            session.run('iinit', None, environment.auth)
+        elif getattr(settings, 'IRODS_GLOBAL_SESSION', False):
+            session = GLOBAL_SESSION
+        elif icommands.ACTIVE_SESSION:
+            session = icommands.ACTIVE_SESSION
+        else:
+            raise KeyError('settings must have IRODS_GLOBAL_SESSION set if there is no environment object')
+
     if split_path_strs[0] == 'bags':
         res_id = os.path.splitext(split_path_strs[1])[0]
     else:
@@ -32,12 +61,15 @@ def download(request, path, *args, **kwargs):
         return response
 
     # do on-demand bag creation
-    istorage = IrodsStorage()
     bag_modified = "false"
     # needs to check whether res_id collection exists before getting/setting AVU on it to accommodate the case
     # where the very same resource gets deleted by another request when it is getting downloaded
-    if istorage.exists(res_id):
-        bag_modified = istorage.getAVU(res_id, 'bag_modified')
+    if federated_path:
+        res_root = '{}/{}'.format(federated_path, res_id)
+    else:
+        res_root = res_id
+    if istorage.exists(res_root):
+        bag_modified = istorage.getAVU(res_root, 'bag_modified')
     if bag_modified == "true":
         ret_status = create_bag_by_irods(res_id, istorage)
         # only reset bag_modified to true when create_bag_by_irods succeeds.
@@ -45,21 +77,8 @@ def download(request, path, *args, **kwargs):
         # as a workaround since race condition is not handled on iRODS side for
         # bag creation and we don't want to show race condition-related iRODs exceptions
         # when bag download can still proceed successfully.
-        if istorage.exists(res_id) and ret_status:
-            istorage.setAVU(res_id, 'bag_modified', "false")
-
-    if 'environment' in kwargs:
-        environment = int(kwargs['environment'])
-        environment = m.RodsEnvironment.objects.get(pk=environment)
-        session = Session("/tmp/django_irods", settings.IRODS_ICOMMANDS_PATH, session_id=uuid4())
-        session.create_environment(environment)
-        session.run('iinit', None, environment.auth)
-    elif getattr(settings, 'IRODS_GLOBAL_SESSION', False):
-        session = GLOBAL_SESSION
-    elif icommands.ACTIVE_SESSION:
-        session = icommands.ACTIVE_SESSION
-    else:
-        raise KeyError('settings must have IRODS_GLOBAL_SESSION set if there is no environment object')
+        if istorage.exists(res_root) and ret_status:
+            istorage.setAVU(res_root, 'bag_modified', "false")
 
     # obtain mime_type to set content_type
     mtype = 'application-x/octet-stream'
