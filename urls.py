bc941455837962af552cf3ce76e74b1b265bbee0
hyi <hongyi@renci.org>
2016-08-09 17:57:48 -0400

    changes required to get async download working

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     # users API
 
     url(r'^download/(?P<path>.*)$', 'django_irods.views.download'),
-    url(r'^list/$', 'django_irods.views.list'),
+    url(r'^poll_for_download/$', 'django_irods.views.poll_for_download'),
 )
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from uuid import uuid4
 import os
+import json
 import mimetypes
 
 from rest_framework.decorators import api_view
@@ -8,15 +9,14 @@ from rest_framework.decorators import api_view
 from django_irods import icommands
 from django_irods.storage import IrodsStorage
 from django.conf import settings
-from django.http import HttpResponse, FileResponse
+from django.http import HttpResponse, FileResponse, HttpResponseRedirect
 
 from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
-from hs_core.hydroshare.hs_bagit import create_bag_by_irods
+from hs_core.tasks import create_bag_by_irods
 from hs_core.hydroshare.resource import FILE_SIZE_LIMIT
 
 from . import models as m
 from .icommands import Session, GLOBAL_SESSION
-
 
 @api_view(['GET'])
 def download(request, path, *args, **kwargs):
@@ -79,14 +79,11 @@ def download(request, path, *args, **kwargs):
         if istorage.exists(res_root):
             bag_modified = istorage.getAVU(res_root, 'bag_modified')
         if bag_modified == "true":
-            ret_status = create_bag_by_irods(res_id, istorage)
-            # only reset bag_modified to true when create_bag_by_irods succeeds.
-            # At this point the irods exception is not populated to the web front end
-            # as a workaround since race condition is not handled on iRODS side for
-            # bag creation and we don't want to show race condition-related iRODs exceptions
-            # when bag download can still proceed successfully.
-            if istorage.exists(res_root) and ret_status:
-                istorage.setAVU(res_root, 'bag_modified', "false")
+            task = create_bag_by_irods.apply_async((res_id, istorage),
+                                                  countdown=3)
+            request.session['task_id'] = task.task_id
+            request.session['download_path'] = request.path
+            return HttpResponseRedirect(res.get_absolute_url())
 
     # obtain mime_type to set content_type
     mtype = 'application-x/octet-stream'
@@ -98,10 +95,11 @@ def download(request, path, *args, **kwargs):
     stdout = session.run("ils", None, "-l", path)[0].split()
     flen = int(stdout[3])
     if flen <= FILE_SIZE_LIMIT:
-        options = ('-',) # we're redirecting to stdout.
+        options = ('-',)  # we're redirecting to stdout.
         proc = session.run_safe('iget', None, path, *options)
         response = FileResponse(proc.stdout, content_type=mtype)
-        response['Content-Disposition'] = 'attachment; filename="{name}"'.format(name=path.split('/')[-1])
+        response['Content-Disposition'] = 'attachment; filename="{name}"'.format(
+            name=path.split('/')[-1])
         response['Content-Length'] = flen
         return response
     else:
@@ -111,20 +109,17 @@ def download(request, path, *args, **kwargs):
         return response
 
 
-def list(request, *args, **kwargs):
-    if 'environment' in kwargs:
-        environment = int(kwargs['environment'])
-        environment = m.RodsEnvironment.objects.get(pk=environment)
-        session = Session("/tmp/django_irods", settings.IRODS_ICOMMANDS_PATH, session_id=uuid4())
-        session.create_environment(environment)
-        session.run('iinit', None, environment.auth)
-    elif getattr(settings, 'IRODS_GLOBAL_SESSION', False):
-        session = GLOBAL_SESSION
-    else:
-        raise KeyError('settings must have IRODS_GLOBAL_SESSION set if there is no environment object')
-
-    options = ('-',) # we're redirecting to stdout.
-
-    proc = session.run_safe('ils', None, *options)
-    response = HttpResponse(proc.stdout)
-    return response
+def poll_for_download(request, *args, **kwargs):
+    '''
+    A view function to tell the client if the asynchronous create_bag_by_irods()
+    task is done and the bag file is ready for download.
+    Args:
+        request: an ajax request to check for download status
+    Returns:
+        JSON response to return result from asynchronous task create_bag_by_irods
+    '''
+    task_id = request.POST.get("task_id")
+    result = create_bag_by_irods.AsyncResult(task_id)
+    if result.ready():
+        return HttpResponse(json.dumps({"status": result.get()}))
+    return HttpResponse(json.dumps({"status": None}))
