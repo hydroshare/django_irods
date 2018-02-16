f3920946babe1b4665f2365f8c99f5bd9ff5df4b
sblack-usu <scott.black@usu.edu>
2018-02-16 09:52:30 -0700

    [#2567] - check is resource exists and return bad responsse if it does not

--- views.py
+++ views.py
@@ -1,24 +1,22 @@
-from uuid import uuid4
-import os
 import json
 import mimetypes
+import os
 import random
+from uuid import uuid4
 
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse, FileResponse, HttpResponseRedirect, HttpResponseBadRequest
 from rest_framework.decorators import api_view
 
 from django_irods import icommands
 from django_irods.storage import IrodsStorage
-from django.conf import settings
-from django.http import HttpResponse, FileResponse, HttpResponseRedirect
-from django.core.exceptions import PermissionDenied
-
-from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
-from hs_core.tasks import create_bag_by_irods, create_temp_zip, delete_zip
-from hs_core.hydroshare.resource import FILE_SIZE_LIMIT
-from hs_core.signals import pre_download_file, pre_check_bag_flag
 from hs_core.hydroshare import check_resource_type
 from hs_core.hydroshare.hs_bagit import create_bag_files
-
+from hs_core.hydroshare.resource import FILE_SIZE_LIMIT
+from hs_core.signals import pre_download_file, pre_check_bag_flag
+from hs_core.tasks import create_bag_by_irods, create_temp_zip, delete_zip
+from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
 from . import models as m
 from .icommands import Session, GLOBAL_SESSION
 
@@ -78,33 +76,14 @@ def download(request, path, rest_call=False, use_async=True, *args, **kwargs):
     else:
         res_root = res_id
 
-    bag_modified = "false"
-    metadata_dirty = "false"
-    if istorage.exists(res_root) and not is_zip_download:
-        bag_modified = istorage.getAVU(res_root, 'bag_modified')
-        # make sure if bag_modified is not set to true, we still recreate the bag if the
-        # bag file does not exist for some reason to resolve the error to download a nonexistent
-        # bag when bag_modified is false due to the flag being out-of-sync with the real bag status
-        if bag_modified is None or bag_modified.lower() == "false":
-            # check whether the bag file exists
-            bag_file_name = res_id + '.zip'
-            if res_root.startswith(res_id):
-                bag_full_path = os.path.join('bags', bag_file_name)
-            else:
-                bag_full_path = os.path.join(federated_path, 'bags', bag_file_name)
-            # set bag_modified to 'true' if the bag does not exist so that it can be recreated
-            # and the bag_modified AVU will be set correctly as well subsequently
-            if not istorage.exists(bag_full_path):
-                bag_modified = 'true'
-        metadata_dirty = istorage.getAVU(res_root, 'metadata_dirty')
-
-
+    if not istorage.exists(res_root):
+        return HttpResponseBadRequest("ResourceId {res_id} does not exist".format(res_id=res_id));
 
     if is_zip_download:
 
-        if not path.endswith(".zip"): # requesting folder that needs to be zipped
+        if not path.endswith(".zip"):  # requesting folder that needs to be zipped
             input_path = path.split(res_id)[1]
-            random_hash = random.getrandbits(16)
+            random_hash = random.getrandbits(32)
             random_hash_path = 'zips/{res_id}/{rand_folder}'.format(res_id=res_id, rand_folder=random_hash)
             output_path = '{random_hash_path}{path}.zip'.format(random_hash_path=random_hash_path, path=input_path)
 
@@ -135,7 +114,24 @@ def download(request, path, rest_call=False, use_async=True, *args, **kwargs):
 
             path = output_path
 
+    metadata_dirty = "false"
     if is_bag_download:
+        bag_modified = istorage.getAVU(res_root, 'bag_modified')
+        # make sure if bag_modified is not set to true, we still recreate the bag if the
+        # bag file does not exist for some reason to resolve the error to download a nonexistent
+        # bag when bag_modified is false due to the flag being out-of-sync with the real bag status
+        if bag_modified is None or bag_modified.lower() == "false":
+            # check whether the bag file exists
+            bag_file_name = res_id + '.zip'
+            if res_root.startswith(res_id):
+                bag_full_path = os.path.join('bags', bag_file_name)
+            else:
+                bag_full_path = os.path.join(federated_path, 'bags', bag_file_name)
+            # set bag_modified to 'true' if the bag does not exist so that it can be recreated
+            # and the bag_modified AVU will be set correctly as well subsequently
+            if not istorage.exists(bag_full_path):
+                bag_modified = 'true'
+        metadata_dirty = istorage.getAVU(res_root, 'metadata_dirty')
         # do on-demand bag creation
         # needs to check whether res_id collection exists before getting/setting AVU on it
         # to accommodate the case where the very same resource gets deleted by another request
@@ -194,12 +190,12 @@ def download(request, path, rest_call=False, use_async=True, *args, **kwargs):
         proc = session.run_safe('iget', None, path, *options)
         response = FileResponse(proc.stdout, content_type=mtype)
         response['Content-Disposition'] = 'attachment; filename="{name}"'.format(
-                                                        name=path.split('/')[-1])
+            name=path.split('/')[-1])
         response['Content-Length'] = flen
         return response
     else:
         content_msg = "File larger than 1GB cannot be downloaded directly via HTTP. " \
-                       "Please download the large file via iRODS clients."
+                      "Please download the large file via iRODS clients."
         response = HttpResponse(status=403)
         if rest_call:
             response.content = content_msg
